a7ce9501cb854549b7a0e6961cf20d7b19fc064b
enriver <riverkeh@naver.com>
2021-03-10 15:06:59 +0900

    6/6

---
+++ Tree_Inorder.py
@@ -0,0 +1,64 @@
+# Tree: Inorder Traversal
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def inOrder(root):
+    def _in_order(root):
+        if root is None:
+            pass
+        else:
+            _in_order(root.left)
+            print(root.info,end=' ')
+            _in_order(root.right)
+    _in_order(root)
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+inOrder(tree.root)
