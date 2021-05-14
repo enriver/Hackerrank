d896709a77d1df81317d2360172ddc214efc723a
enriver <riverkeh@naver.com>
2021-05-15 01:24:48 +0900

    13/13

--- XOR.py
+++ XOR.py
@@ -1,23 +1,14 @@
 # SUM vs XOR
-
-import math
-import os
-import random
-import re
-import sys
+'''
+https://www.snoopybox.co.kr/1863 풀이
+'''
 
 # Complete the sumXor function below.
 def sumXor(n):
-    x=0
-    res=0
-
-    while x<=n:
-        if n+x==n^x:
-            res+=1
-
-        x+=1
-
-    return res
+    if n==0:
+        return 1
+    else:
+        return 2**(bin(n).count('0')-1)
 
 if __name__ == '__main__':
     n = int(input().strip())
